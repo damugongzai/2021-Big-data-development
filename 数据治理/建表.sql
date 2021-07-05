8b691075c11e4f46e8bf749d3f736f16277d8635
damugongzai <43875460+damugongzai@users.noreply.github.com>
2021-07-06 00:07:53 +0800

    final

--- 数据治理/建表.sql
+++ 数据治理/建表.sql
@@ -1,23 +1,38 @@
-按月查看城市、商品类别的销售额
-select to_char(data_yys.sell.buy_time , 'YYYY-MM'), data_yys.sell.sell_city,data_yys.goods.kind , sum(data_yys.sell.total_price) as total_price
-from data_yys.sell left join data_yys.goods on data_yys.sell.goods_id = data_yys.goods.goods_id 
-group by to_char(data_yys.sell.buy_time , 'YYYY-MM'), data_yys.sell.sell_city, data_yys.goods.kind
-按月查看总体销售额、新增注册人数
-select to_char(data_yys.sale.par_time, 'YYYY-MM') as year_month,sum(data_yys.sale.sale_price) as sum_sale from data_yys.sale group by to_char(data_yys.sale.par_time, 'YYYY-MM');
-select to_char(data_yys.client.reg_time, 'YYYY-MM') as year_month,count(*) as client_count from data_yys.client group by to_char(data_yys.client.reg_time, 'YYYY-MM');
-select data_yys.sale.sale_city as city,to_char(data_yys.sale.par_time, 'YYYY-MM') as year_month,sum(data_yys.sale.sale_price) as sum_sale from data_yys.sale group by to_char(data_yys.sale.par_time, 'YYYY-MM'),data_yys.sale.sale_city;
-select to_char(data_yys.sale.par_time, 'YYYY-MM') as year_month,data_yys.sale.sale_city as city,data_yys.commodity.cate as com_cate,sum(data_yys.sale.sale_price) as sum_sale 
-from data_yys.sale left join data_yys.commodity on data_yys.sale.com_code = data_yys.commodity.code
-group by to_char(data_yys.sale.par_time, 'YYYY-MM'),data_yys.sale.sale_city,data_yys.commodity.cate;
-select to_char(data_yys.sale.par_time, 'YYYY-MM') as year_month,data_yys.sale.sale_sex as sex,data_yys.commodity.cate as com_cate,sum(data_yys.sale.sale_price) as sum_sale 
-from data_yys.sale left join data_yys.commodity on data_yys.sale.com_code = data_yys.commodity.code
-group by to_char(data_yys.sale.par_time, 'YYYY-MM'),data_yys.sale.sale_sex,data_yys.commodity.cate;
+drop table if exists data_yys.store;
+create table data_yys.store
+(
+  code VARCHAR(4) PRIMARY KEY
+);
 
-INSERT INTO "data_yys"."client" VALUES ('101', 'aaa', '1841860154', 'uf0ZVjqq.com',10,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('102', 'aoa', '18415154', 'uf0ZVjqq@.com',9,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('103', 'kaa', '15244', 'uf0ZVjqq',8,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('104', 'ayfa', '1841864', 'uf0ZVjqq.@com',7,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('105', 'nba', '184154', 'uf0ZVjqq.com',6,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('106', 'aba', '18418d54', 'uf0ZVjqq.com',5,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('107', 'afa', '11860146764', 'uf0ZVjqq.com',4,0,'2020-07-05 12:43:01');
-INSERT INTO "data_yys"."client" VALUES ('108', 'ahtkya', '115235y53', 'uf0ZVjqq.com',3,0,'2020-07-05 12:43:01');
+drop table if exists data_yys.client;
+create table data_yys.client
+(
+  code VARCHAR(10) PRIMARY KEY,
+  cli_name VARCHAR(10),
+  phone_number   VARCHAR(11),
+  email VARCHAR(100),
+  cli_age INT,
+  sex INT,
+  reg_time TIMESTAMP
+);
+
+
+drop table if exists data_yys.commodity;
+create table data_yys.commodity
+(
+  code VARCHAR(4) PRIMARY KEY,
+  cate VARCHAR(4),
+  price INT
+);
+
+drop table if exists data_yys.sale;
+create table data_yys.sale
+(
+  sale_code VARCHAR(10) PRIMARY KEY,
+  sale_city VARCHAR(10),
+  com_code VARCHAR(4),
+  par_time TIMESTAMP,
+  sale_sex INT,
+  sale_price INT,
+  foreign key(com_code) references data_yys.commodity(code)
+);
